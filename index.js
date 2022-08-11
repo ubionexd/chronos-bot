8f80590d9eb4ad7f1ab4fdd0490ae03d5dbc8846
ubionexd <ubionexd@gmail.com>
2022-08-11 00:52:55 +0000

    Done

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
+const token = process.env.TOKEN;
+
+client.once('ready', () => {
+	console.log('Startup succeeded!');
+});
+
+client.on('guildMemberAdd', (member) => {
+	const name = member.user.username;
+	
+	console.log(`${name} joined, sending message`);
+
+	const message = new EmbedBuilder()
+		.setTitle(`Welcome ${name} to`)
+		.setDescription(`Chronos Client's Official Discord server. Nice to see you!\nIf you need some help, ask your question in <#976199262826270730>`)
+		.setAuthor({name: 'Chronos Client', iconURL: 'https://cdn.modrinth.com/data/qBOrMQj2/2a2dd095e3d20fc0607f6d48939ae235ec450b0b.png', url: 'https://modrinth.com/modpack/chronos'});
+
+	member.send({embeds: [message]});
+});
+
+client.login(token);
