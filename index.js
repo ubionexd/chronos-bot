60fb461a8edc856998b1e6ed356a8a31fe0917cd
UbiOne <ubionexd@gmail.com>
2022-08-12 13:16:01 +0200

    Is this even getting the token or not?

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
-const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
-require("dotenv").config();
+/* const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js')
+require('dotenv').config()
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
-});
+})
 
-client.once("ready", () => {
-  console.log("Startup succeeded!");
-});
+client.once('ready', () => {
+  console.log('Startup succeeded!')
+})
 
-client.on("guildMemberAdd", (member) => {
-  const name = member.user.username;
+client.on('guildMemberAdd', (member) => {
+  const name = member.user.username
 
-  console.log(`${name} joined, sending message`);
+  console.log(`${name} joined, sending message`)
 
   const message = new EmbedBuilder()
     .setTitle(`Welcome ${name} to`)
     .setDescription(
-      `Chronos Client's Official Discord server. Nice to see you!\nIf you need some help, ask your question in <#976199262826270730>`
+      `Chronos Client's Official Discord server. Nice to see you!\nIf you need some help, ask your question in <#976199262826270730>`,
     )
     .setAuthor({
-      name: "Chronos Client",
+      name: 'Chronos Client',
       iconURL:
-        "https://cdn.modrinth.com/data/qBOrMQj2/2a2dd095e3d20fc0607f6d48939ae235ec450b0b.png",
-      url: "https://modrinth.com/modpack/chronos",
-    });
+        'https://cdn.modrinth.com/data/qBOrMQj2/2a2dd095e3d20fc0607f6d48939ae235ec450b0b.png',
+      url: 'https://modrinth.com/modpack/chronos',
+    })
 
-  member.send({ embeds: [message] });
-});
-
-client.login('process.env.DEPLOY_TOKEN');
+  member.send({ embeds: [message] })
+})
+*/
+console.log('process.env.DEPLOY_TOKEN')
+// client.login('process.env.DEPLOY_TOKEN')
