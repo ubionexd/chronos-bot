160ec3ff3ea25d34e58e3522795323fb5c7493e5
UbiOne <ubionexd@gmail.com>
2022-10-10 20:45:22 +0200

    Updating presence & Formatting
    Added a custom presence (status) for the bot which displays the member count - 3 (so it doesn't count me and the two bots on the server) and used Prettier to format the code.

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
-const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js')
-const moment = require('moment')
-require('dotenv').config()
+const {
+  Client,
+  GatewayIntentBits,
+  EmbedBuilder,
+  ActivityType,
+} = require("discord.js");
+const moment = require("moment");
+require("dotenv").config();
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
-})
+});
 
-client.once('ready', () => {
-  console.log('Startup succeeded!')
-})
+client.once("ready", () => {
+  setInterval(() => {
+    const guildCount = client.guilds.cache.map((g) => {
+      return g.memberCount;
+    });
+    const members = guildCount - 3;
+    client.user.setActivity(`${members} members`, {
+      type: ActivityType.Watching,
+    });
+    
+    console.log(`Update presence!\nTotal members: ${guildCount}\nReal members: ${members}`)
+  }, 5000);
 
-client.on('guildMemberAdd', (member) => {
-  const name = member.user.username
-  const id = member.user.id
-  let time = moment.utc().format('YYYY.MM.DD hh:mm')
-  console.log(`${name} (${id}) joined. Sending message...`)
+  console.log("Startup succeeded!");
+});
+
+client.on("guildMemberAdd", (member) => {
+  const name = member.user.username;
+  const id = member.user.id;
+  let time = moment.utc().format("YYYY.MM.DD hh:mm");
+  console.log(`${name} (${id}) joined. Sending message...`);
 
   const message = new EmbedBuilder()
     .setTitle(`Welcome ${name} to`)
     .setDescription(
-      `Chronos Client's Official Discord server. Nice to see you!\nIf you need some help, ask your question in <#976199262826270730>`,
+      `Chronos Client's Official Discord server. Nice to see you!\nIf you need some help, ask your question in <#976199262826270730>`
     )
     .setAuthor({
-      name: 'Chronos Client',
+      name: "Chronos Client",
       iconURL:
-        'https://cdn.modrinth.com/data/qBOrMQj2/2a2dd095e3d20fc0607f6d48939ae235ec450b0b.png',
-      url: 'https://modrinth.com/modpack/chronos',
+        "https://cdn.modrinth.com/data/qBOrMQj2/2a2dd095e3d20fc0607f6d48939ae235ec450b0b.png",
+      url: "https://modrinth.com/modpack/chronos",
     })
     .setColor(0xfa824c)
-    .setFooter({ text: `Joined at ${time} UTC` })
+    .setFooter({ text: `Joined at ${time} UTC` });
 
-  member.send({ embeds: [message] })
-})
+  member.send({ embeds: [message] });
+});
 
-client.login(process.env.DEPLOY_TOKEN)
+client.login(process.env.DEV_TOKEN);
